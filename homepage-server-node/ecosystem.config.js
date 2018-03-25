31859c2a3f5a18f6be787330c267a3dedadc0aaf
Izaak Baker <izaak@Izaaks-MacBook-Air.local>
2018-03-25 18:42:36 -0400

    pm2 stuff??

--- homepage-server-node/ecosystem.config.js
+++ homepage-server-node/ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'http://ec2-34-217-42-71.us-west-2.compute.amazonaws.com/',
+        host: 'ec2-34-217-42-71.us-west-2.compute.amazonaws.com',
         key: '~/.ssh/homepage.pem',
         ref: 'origin/master',
         repo: 'git@github.com:nicepangolin/homepage.git',
-        path: '/home/ubuntu/homepage/homepage-server-node',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        path: '/home/ubuntu/homepage',
+        'post-deploy': 'yarn install && pm2 startOrRestart ecosystem.config.js'
       }
     }
   }
