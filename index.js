58ff24f6c1444c2de0ac6056070a06c46d298c02
Izaak Baker <izaak@Izaaks-MacBook-Air.local>
2018-03-25 22:21:00 -0400

    Switch to static hosting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, World!!'));
+app.use(express.static('static'));
 
 app.listen(3000, () => console.log('Up and running!'));
