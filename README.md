d370b0b8a90a3ab8dfdf9ba5a3f6d32f9857ab27
Izaak Baker <izaak@10.64.111.64.naturalwireless.com>
2018-03-31 14:55:58 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Homepage
+========
+
+Personal website of Izaak Baker. Written as an exercise to actually build, deploy, and maintain a web application from scratch. 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    apps: [{
-      name: 'homepage',
-      script: './server/index.js'
-    }],
-    deploy: {
-      production: {
-        user: 'ubuntu',
-        host: 'ize.mba',
-        key: '~/.ssh/homepage.pem',
-        ref: 'origin/master',
-        repo: 'git@github.com:nicepangolin/homepage.git',
-        path: '/home/ubuntu/homepage',
-        'post-deploy': 'pwd && npm install && pm2 startOrRestart ecosystem.config.js'
-      }
+  apps: [{
+    name: 'homepage',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ize.mba',
+      key: '~/.ssh/homepage.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:nicepangolin/homepage.git',
+      path: '/home/ubuntu/homepage',
+      'post-deploy': 'pwd && npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
+}
