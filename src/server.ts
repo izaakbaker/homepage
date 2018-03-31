250fbcc4c9404c3b935ae3d5db52f6a5eedcb5ae
Izaak Baker <izaak@10.64.111.64.naturalwireless.com>
2018-03-31 17:39:51 -0400

    style

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ const app = express();
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (request: Request, response: Response) => {
     response.render('index', {});
 });
